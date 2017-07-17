0c57d1c9bdbebcc1289b47c1d91709f7e4f4745b
djskim27 <djskim27@users.noreply.github.com>
2017-07-16 23:43:31 -0400

    reorganized js files

--- public/js/main.js
+++ public/js/main.js
@@ -1,184 +1,242 @@
-$(function(){
-loadKeyboard();
+$(function() {
 
-const topTierVariables = {
-    pokemon: ['squirtle', 'charmander', 'bulbasaur', 'pikachu', 'machop', 'charizard', 'venusaur', 'jigglypuff'],
-    $wordDiv: $('#word-lines')
+//game variables
+const gameVariables = {
+    randomNumber: 0,
+    randomPokemon: '',
+    lettersInPlay:'',
+    arrayOfSpaces: [],
+    $wordDiv: $('#word-lines'),
+    numberOfAttempts: 6,
+    $attemptsDiv: $('#attempts'),
+    $pokemonImg: $('#pokemon-pic'),
+    $playAgain: $('<div class="btn btn-outline-danger white">Next Pokemon</div>'),
+    $pokeballs: ('<img src="css/images/pokeball.png" class="pokeball-div">'),
+    win: 0,
+    loss: 0,
+
+
+
+};
+//choose random pokemon and display line spaces for letters
+function createPuzzle() {
+
+    gameVariables.randomNumber = Math.floor(pokemon.length * Math.random());
+    gameVariables.randomPokemon = pokemon[gameVariables.randomNumber].name;
+    gameVariables.lettersInPlay = gameVariables.randomPokemon.toUpperCase().split('');
+    //display pokemon image
+     var img =$('<img class="hidden img-size img-fluid" />').attr('src', pokemon[gameVariables.randomNumber].img);
+    img.appendTo(gameVariables.$pokemonImg);
+    //for every letter in pokemon name, create a linespace
+    for (var i=0; i<gameVariables.lettersInPlay.length; i++) {
+
+        gameVariables.arrayOfSpaces.push('_ ');
+
+    };
+    
+    //visibily show linespaces in word-lines div
+    gameVariables.$wordDiv.append(gameVariables.arrayOfSpaces);
+
+    //show the player has 6 attempts
+    gameVariables.$attemptsDiv.html('Pokeballs Remaining:');
+    //player has 6 pokeballs which represent lives
+    erasePokeball();
+    pokeballCounter();
+    //show win and loss numbers in pokebox modal
+    $('.win').html('Caught: ' + gameVariables.win);
+    $('.loss').html('Ran Away: ' + gameVariables.loss);
+    
+
 }
-//words
-    //words array
 
+//create keyboard for game
+function createKeyboard() {
     
+   // var letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
+    var letters1 = 'qwertyuiop'.toUpperCase().split('');
+    var letters2 = 'asdfghjkl'.toUpperCase().split('');
+    var letters3 = 'zxcvbnm'.toUpperCase().split('');
+    var $keyboard1 = $('#keyboard1');
+    var $keyboard2 = $('#keyboard2');
+    var $keyboard3 = $('#keyboard3');
+    for(var i=0; i<letters1.length; i++) {
 
-    var words = topTierVariables.pokemon;
-    var randomNumber = 0;
-    var randomWord = '';
-    var lettersInPlay = '';
-    var arrayOfSpaces=[];
-
-    
-    //take random word from array and split it
-    var randomNumber = Math.floor(words.length * Math.random());
-   
-    var randomWord = words[randomNumber];
-    
-    var lettersInPlay = randomWord.toUpperCase().split('');
-    
-    //linespaces
-    
-    
-  
-    
-    
-    //for every letter in my word, create a ___ to signify linespace
-    
-    for (var i =0; i<lettersInPlay.length; i++) {
+       
+         //var button = $('<div>').addClass('btn').html(letter[i]);
+        $keyboard1.append('<div class="btn btn-outline-primary keys white" id="letter-div">'+letters1[i]+'</div>');
         
-        arrayOfSpaces.push('__ ');
-    
-    
     }
-    createBoard();
-    //Display array of spaces into word-lines
-   // var $wordDiv = $('#word-lines');
-    function createBoard(){
-    topTierVariables.$wordDiv.append(arrayOfSpaces);
-    //create divs for each letter in word
-    //for (var i=0; i<lettersInPlay.length; i++) {
- 
-   // var $wordDiv = $('#word-lines');
-    //$wordDiv.append('<div id="letter-div" class="d-inline">'+lettersInPlay[i]+'</div>')
-    //}
-    
+    for(var i=0; i<letters2.length; i++) {
+
+       
+         //var button = $('<div>').addClass('btn').html(letter[i]);
+        $keyboard2.append('<div class="btn btn-outline-primary keys white" id="letter-div">'+letters2[i]+'</div>');
+        
     }
+    for(var i=0; i<letters3.length; i++) {
+
+       
+         //var button = $('<div>').addClass('btn').html(letter[i]);
+        $keyboard3.append('<div class="btn btn-outline-primary keys white" id="letter-div">'+letters3[i]+'</div>');
+        
+    }
+
+    //adding click function to keys
+$('.keys').on('click', function(event){
+
+    var $keyClickedOn = $(event.target);
+    var $keyText = $keyClickedOn.text();
+    $keyClickedOn.off('click');
+
+    playGame($keyText);
+
+});
     
-    
-
-
-
-//keyboard
-function loadKeyboard(){
-    var letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase().split('');
-    for(var i=0; i<letters.length; i++) {
-    var $keyboard = $('#keyboard')
-    //var button = $('<div>').addClass('btn').html(letter[i]);
-    $keyboard.append('<div class="btn btn-outline-success keys" id="letter-div">'+letters[i]+'</div>');
-}
-}
-
-//Number of Attempts
-var numberOfAttempts = 6;
-
-//create Number of Attempts Div
-var attempts = $('#attempts').html('Attempts Remaining: ' + numberOfAttempts);
-    
-
-
-var letterClicked;
-
-//Check to see if letter is in linespace
-function letterMatch(button) {
-//if letter clicked equals one of the letters in linespace
-    for (var i=0; i<lettersInPlay.length; i++) {
-    if (letterClicked === lettersInPlay[i]) {
-        arrayOfSpaces[i] = letterClicked;
-        // display letter in corresponding linespace
-        topTierVariables.$wordDiv.text(arrayOfSpaces.join(' '));
-        //letter button turns green and is disabled
-        turnKeyGreen();
-        //check for word is complete/correct
-        correctWord();
- 
-    
-    
- 
-   
-    } 
-   }
- // if letter is incorrect
-if (lettersInPlay.indexOf(letterClicked) === (-1)) {
-    // letter button turns red and is disabled
-    turnKeyRed();
-    
-    numberOfAttempts -= 1;
-    $('#attempts').html('Attempts Remaining: ' + numberOfAttempts);
-    userLoses();
-}
 }
 
-//if word is completely correct; give an indicator of win and move to next word
-function correctWord() {
-       if (topTierVariables.$wordDiv.text().replace(/ +/g, "") === randomWord.toUpperCase()) {
-        alert('you won!')
-        } 
-}
+createPuzzle();
+createKeyboard();
+console.log(gameVariables.lettersInPlay);
+console.log(gameVariables.arrayOfSpaces);
 
 
-//if user can't guess the word
-function userLoses() {
-        if (numberOfAttempts === 0) {
-            disableKeyboard();
-            alert('The pokemon got away!');
-            resetGame();
+
+//play game and try to guess/match correct letters for pokemon
+function playGame(x) {
+    //if clicked letter is correct
+    for (var i=0; i<gameVariables.lettersInPlay.length; i++) {
+
+        if (x === gameVariables.lettersInPlay[i]) {
+
+            gameVariables.arrayOfSpaces[i] = x;
+        //display letter in corresponding linespace
+            var newArrayOfSpaces = gameVariables.arrayOfSpaces.join(' ');
+            
+            gameVariables.$wordDiv.text(newArrayOfSpaces);
+        
+        //letter turns green to indicate correct choice
+            $(event.currentTarget).removeClass('btn-outline-primary white');
+            $(event.currentTarget).addClass('btn-success');
+        
+        //check if user guesses word correctly 100 percent
+            if (gameVariables.$wordDiv.text().replace(/ +/g, "") === gameVariables.randomPokemon.toUpperCase()) {
+
+                //show winner user won
+                function winQuote() {alert('You caught ' + gameVariables.randomPokemon + '!');}
+                setTimeout(winQuote,300);
+
+                //reveal pokemon image
+                $('.img-size').removeClass('hidden');
+                //disable keyboard
+                $('.keys').off('click');
+                //store caught pokemon in pokebox
+                var pokeGif = $('<img class="gif-size">').attr('src', pokemon[gameVariables.randomNumber].gif);
+
+                pokeGif.appendTo($('.modal-body'));
+                //update win stat
+                gameVariables.win += 1;
+                $('.win').html('Caught: ' + gameVariables.win)
+                //display play again option
+                playAgain();
+
+            } 
+        }
+    }
+    //if clicker letter is incorrect
+    if (gameVariables.lettersInPlay.indexOf(x) === -1) {
+
+        //letter turns red and disabled to indicate incorrect choice
+        $(event.currentTarget).removeClass('btn-outline-primary');
+        $(event.currentTarget).addClass('btn-danger disabled');
+        //number of attempts decrease by 1
+        gameVariables.numberOfAttempts -= 1;
+        //update pokeball
+        erasePokeball();
+        pokeballCounter();
+        //if user's attempts reach 0, user loses game.
+        if (gameVariables.numberOfAttempts === 0) {
+
+            //disable keyboard
+            $('.keys').off('click');
+
+            //show the pokemon's identity name
+            gameVariables.$wordDiv.empty();
+            gameVariables.$wordDiv.html(gameVariables.lettersInPlay.join(' '));
+
+            //indicate that opponent has lost
+            function runAway(){
+                alert(gameVariables.randomPokemon +  ' ran away!')
+            };
+            setTimeout(runAway,300);
+            //play again button appears
+            playAgain();
+             //update loss stat
+            gameVariables.loss += 1
+            $('.loss').html('Ran Away: ' + gameVariables.loss);
+            
+            
         }
 
+    }
+    
 }
-//reset Game
-function resetGame() {
-            topTierVariables.$wordDiv.empty();
-            arrayOfSpaces = [];
-            numberOfAttempts = 6;
+//pressing play again button allows user to reload new puzzle
+function playAgain(){
 
-            $('#attempts').html('Attempts Remaining: ' + numberOfAttempts);
-            
-            $('.keys').removeClass('btn-success btn-danger disabled');
-            $('.keys').removeClass('btn-danger');
-            $('.keys').removeClass('disabled');
-            $('.keys').addClass('btn-outline-success')
-           
-}
+    //attach play again button below keyboard
+    function playAgainButton() {
+        $('#pokebox').append(gameVariables.$playAgain);
+    };
+    setTimeout(playAgainButton,400);
+    //click event for play again button
+    gameVariables.$playAgain.on('click', function(event){
+        //remove button
+        $(event.currentTarget).remove();
+        //reset game variables
+        gameVariables.randomNumber = 0;
+        gameVariables.randomPokemon = '';
+        gameVariables.lettersInPlay = '';
+        gameVariables.arrayOfSpaces = [];
+        gameVariables.numberOfAttempts = 6;
+        //empty gamepuzzle
+        gameVariables.$wordDiv.empty();
+        gameVariables.$pokemonImg.empty();
+        gameVariables.$attemptsDiv.empty();
+        //empty keyboard
+        $('#keyboard1').empty();
+        $('#keyboard2').empty();
+        $('#keyboard3').empty();
+        //load gamepuzzle
+        createPuzzle();
+        //load keyboard
+        createKeyboard();
 
+        });
 
-//turns key green
-function turnKeyGreen() {
-    $(event.currentTarget).removeClass('btn-outline-success');
-    $(event.currentTarget).addClass('btn-success');
-    //$(event.currentTarget).off('click');
-}
-//turns key red
-function turnKeyRed() {
-    $(event.currentTarget).removeClass('btn-outline-success');
-    $(event.currentTarget).addClass('btn-danger');
-    //$(event.currentTarget).off('click');
-}
+    
+    }
+    //pokeball life counter
+    function pokeballCounter() {
+        for (var i=0; i<gameVariables.numberOfAttempts; i++) {
+               $('#pokeballs').append(gameVariables.$pokeballs)
+        }
+    }
+    //erase pokeball div
+    function erasePokeball() {
+        $('#pokeballs').empty();
+    }
+    //pokebox modal 
+    $('#pokeboxModal').on('shown.bs.modal', function () {
+        $('#pokeboxTitle').focus()
+    })
+    //restart the game and release all pokemon
+    $('#restart').on('click', function(){
+        alert('All your pokemon have been released!');
+        location.reload();
+        
 
-//disable keyboard
-function disableKeyboard () {
-    $('.keys').off('click');
-}
-
-//reveal word after losing/winning
-function revealWord() {} {
-
-}
-
-
-//click eventlisteners
-//
-$('.keys').on('click', function(event){
-$(event.currentTarget).off('click');
-$(event.currentTarget).addClass('disabled');
-letterClicked = $(event.currentTarget).text();
-letterMatch(event.currentTarget);
-
-
-
+    });
+    
+  
 
 });
-
-
-
-});
-
-
-
---
+++ public/js/pokemon.js
@@ -0,0 +1,44 @@
+//array of pokemon objects
+const pokemon = [
+    {
+        name: 'Bulbasaur',
+        img:'https://vignette4.wikia.nocookie.net/nintendo/images/4/43/Bulbasaur.png/revision/latest?cb=20141002083518&path-prefix=en',
+        gif: 'http://www.pokestadium.com/sprites/black-white/animated/bulbasaur.gif'
+    }, 
+    {
+        name: 'Charmander',
+        img:'https://vignette1.wikia.nocookie.net/nintendo/images/5/56/Charmander.png/revision/latest?cb=20141002083351&path-prefix=en',
+        gif: 'http://www.pokestadium.com/sprites/black-white/animated/charmander.gif'
+
+    },
+    {
+        name: 'Squirtle',
+        img:'https://vignette2.wikia.nocookie.net/nintendo/images/e/e3/Squirtle.png/revision/latest?cb=20141002083243&path-prefix=en',
+        gif: 'http://www.pokestadium.com/sprites/black-white/animated/squirtle.gif'
+    },
+    {
+        name: 'Pikachu',
+        img:'https://vignette2.wikia.nocookie.net/nintendo/images/7/77/Pikachu.png/revision/latest?cb=20141002082401&path-prefix=en',
+        gif: 'http://www.pokestadium.com/sprites/black-white/animated/pikachu.gif'
+    },
+    {
+        name: 'Articuno',
+        img: 'https://vignette1.wikia.nocookie.net/nintendo/images/5/52/Articuno.png/revision/latest?cb=20141002090055&path-prefix=en',
+        gif: 'http://www.pokestadium.com/sprites/black-white/animated/articuno.gif'
+    },
+    {
+        name: 'Moltres',
+        img: 'https://vignette2.wikia.nocookie.net/nintendo/images/0/01/Moltres.png/revision/latest?cb=20141002090414&path-prefix=en',
+        gif: 'http://www.pokestadium.com/sprites/black-white/animated/moltres.gif'
+    },
+    {
+        name: 'Zapdos',
+        img: 'https://vignette2.wikia.nocookie.net/nintendo/images/d/d8/Zapdos.png/revision/latest?cb=20141002090124&path-prefix=en',
+        gif: 'http://www.pokestadium.com/sprites/black-white/animated/zapdos.gif'
+    },
+    {
+        name: 'Charizard',
+        img: 'https://vignette2.wikia.nocookie.net/nintendo/images/9/95/Charizard.png/revision/latest?cb=20141002083306&path-prefix=en',
+        gif: 'http://www.pokestadium.com/sprites/black-white/animated/charizard.gif'
+    }
+];
